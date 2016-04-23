664622e6811288f6f2fff71f9e38c959c999f121
gruprog <gruprog@gmail.com>
2016-04-23 19:39:52 -0400

    snapshot

---
+++ students-courses-app/public/MainController.js
@@ -0,0 +1,12 @@
+(function() {
+	var studentsApp = angular.module('studentsApp', []);
+
+	studentsApp.controller('MainController', function($scope) {
+		var student = {
+			name: "Albert Einstein",
+			profilepic: "http://localhost:3000/images/profile/einstein.jpg"
+		};
+
+		$scope.student = student;
+	});
+}());
