2c0af0e4a967b57d117e28368bcfe578d4f4cb8f
gruprog <gruprog@gmail.com>
2016-04-24 08:40:43 -0400

    changes and snapshots

--- students-courses-app/public/MainController.js
+++ students-courses-app/public/MainController.js
@@ -1,12 +1,15 @@
 (function() {
 	var studentsApp = angular.module('studentsApp', []);
 
-	studentsApp.controller('MainController', function($scope) {
-		var student = {
-			name: "Albert Einstein",
-			profilepic: "http://localhost:3000/images/profile/einstein.jpg"
-		};
+	studentsApp.controller('MainController', function($scope, $http) {
+		var onStudentsFetchComplete = function(response) {
+			$scope.students = response.data;
+		}
 
-		$scope.student = student;
+		var onErrorHandler = function(e) {
+			$scope.error = "Error occurred in fetching the students datad";
+		}
+
+		$http.get("http://localhost:3000/students").then(onStudentsFetchComplete, onErrorHandler);		
 	});
 }());
