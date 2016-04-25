6316cebe771cf788c761b912cdc8c7f097eaeb0f
gruprog <gruprog@gmail.com>
2016-04-25 10:44:04 -0400

    adding another test case

--- courses-app/spec/courses/api-service.spec.js
+++ courses-app/spec/courses/api-service.spec.js
@@ -1,11 +1,21 @@
 describe('courses api service', function() {
-  var courses = {
+  var coursesExpected = {
     "courses": [
       {
         "Title": "Beginning AngularJS",
         "ID": "Ang15",
         "Category": "JavaScript",
       },
+      {
+        "Title": "Beginning Android",
+        "ID": "And20",
+        "Category": "Java",
+      }
+    ]
+  };
+
+  var coursesByCategoryExpected = {
+    "courses": [
       {
         "Title": "Beginning Android",
         "ID": "And20",
@@ -23,6 +33,18 @@ describe('courses api service', function() {
       coursesAPI = _coursesAPI_;
     });
 
-    expect(coursesAPI.get('courses')).toEqual(courses);
+    expect(coursesAPI.get('courses')).toEqual(coursesExpected);
   })
+});
+
+it('should return all courses of a category', function() {
+  var coursesAPI = {};
+
+  angular.mock.module('coursesModule');
+
+  angular.mock.inject(function(_coursesAPI_) {
+    coursesAPI = _coursesAPI_;
+  });
+
+  expect(coursesAPI.findCoursesByCategory('Java')).toEqual(coursesByCategoryExpected);
 });
